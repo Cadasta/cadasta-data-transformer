4d849ef23926f9576dd0d8bce356b995008bb32e
DBaah <dbaah@spatialdev.com>
2015-08-12 12:20:34 -0700

    add function comments; update variable names
    change survey references to field_data

--- processes/processform.js
+++ processes/processform.js
@@ -14,17 +14,24 @@
  */
 
 var pg = require('../pg.js'); //
-var DataProcessor = require('../processes/processdata');
+//var DataProcessor = require('../processes/processdata');
 var forEach = require('async-foreach').forEach;
 
 var survey = module.exports = {};
 
 var section_id = null;
 var group_id;
-var survey_id;
+var field_data_id;
 var metadata;
 var formdata;
 
+/**
+ *
+ * @param form
+ * @param callback
+ *
+ * Grab raw json to send to createSurvey
+ */
 survey.processForm = function (form, callback) {
 
     metadata = form.metadata;
@@ -34,12 +41,12 @@ survey.processForm = function (form, callback) {
     var name = pg.sanitize(metadata.name);
     var title = pg.sanitize(metadata.title);
 
-    createSurvey(idString, name, title, function (error, id) {
+    createFieldData(idString, name, title, function (error, id) {
 
-        survey_id = id;
+        field_data_id = id;
 
         if (error.length === 0 && id !== null) {
-            //console.log("Survey successfully created.");
+            console.log("Survey successfully created. id: " + field_data_id);
 
 
             //call recursive function
@@ -47,21 +54,26 @@ survey.processForm = function (form, callback) {
 
             node_handler(metadata.children,null);
 
-            //DataProcessor.process_data(survey_id,formdata);
-            callback(survey_id);
-
-            //parent_handler(json,function(){
-            //
-            //});
+            //DataProcessor.process_data(field_data_id,formdata);
+            callback(field_data_id);
         }
 
     });
 
 };
 
+/**
+ *
+ * Recursive function that loops through question and handles them by type
+ * Function is recursively called if question is a group, using group_id as parent_id in parameter
+ *
+ * @param children
+ * @param parent_id
+ * @param itemDone
+ */
 var node_handler = function(children,parent_id,itemDone){
 
-    children.forEach(function(child_node, index, arr){
+    children.forEach(function(child_node, index){
 
         var name = child_node.name || null;
         var label = child_node.label || null;
@@ -71,9 +83,7 @@ var node_handler = function(children,parent_id,itemDone){
                 // create note
                 note_handler(child_node.name,child_node.label, function (section_id) {
                     //console.log('Note Section created for survey: ' + section_id);
-                    //var async = setTimeout(function(){
-                    //    done();
-                    //}, 500);
+
                 });
 
                 break;
@@ -82,11 +92,11 @@ var node_handler = function(children,parent_id,itemDone){
                 // hand parents children
                 //console.log('Group');
 
-                forEach(children,function(item,index,arr){
-                    var done = this.async();
-                    group_handler(done,child_node.children,parent_id, name, label);
+                //forEach(children,function(item,index,arr){
+                //    var done = this.async();
+                    group_handler(child_node.children,parent_id, name, label);
 
-                });
+                //});
 
             case 'select one':
             case 'select all that apply':
@@ -100,9 +110,7 @@ var node_handler = function(children,parent_id,itemDone){
                             // create new option rows
                             option_handler(child_node.children, question_id, name, label,
                                 function (c) {
-                                    //var async = setTimeout(function(){
-                                    //    done();
-                                    //}, 500);
+
                                 });
                         }
 
@@ -113,9 +121,7 @@ var node_handler = function(children,parent_id,itemDone){
                 // handle normal question types; these types do not have children, therefore a new question row is created
                 get_type_id(child_node.type,function(type_id){
                     question_handler(name, label, type_id, parent_id, function (question_id) {
-                        //var async = setTimeout(function(){
-                        //    done();
-                        //}, 500);
+
                     });
                 })
 
@@ -125,23 +131,31 @@ var node_handler = function(children,parent_id,itemDone){
 
 };
 
-// create new survey in Cadasta DB
-var createSurvey = function (id_string, name, title, callback) {
+/**
+ *
+ * Create new field_data in Cadasta DB from raw json
+ *
+ * @param id_string
+ * @param name
+ * @param title
+ * @param callback return field_data_id
+ */
+var createFieldData = function (id_string, name, title, callback) {
 
     var q1 = 'SELECT * FROM cd_create_field_data(' + (id_string) + ')';
 
     pg.query(q1, function (error, result) {
 
-        survey_id = result[0].cd_create_field_data; // save new survey id
+        field_data_id = result[0].cd_create_field_data; // save new survey id
 
         var updateSurvey = [];
-        updateSurvey.push('UPDATE field_data SET name = ' + name + 'where id=' + survey_id);
-        updateSurvey.push('UPDATE field_data SET label = ' + title + 'where id=' + survey_id);
+        updateSurvey.push('UPDATE field_data SET name = ' + name + 'where id=' + field_data_id);
+        updateSurvey.push('UPDATE field_data SET label = ' + title + 'where id=' + field_data_id);
 
         //execute multiple update statements
         pg.queryArr(updateSurvey, function (error, result) {
             if (result.length == 2) {
-                callback(error, survey_id);
+                callback(error, field_data_id);
             }
         })
 
@@ -149,13 +163,22 @@ var createSurvey = function (id_string, name, title, callback) {
 
 };
 
-// crate new question in Question table
+/**
+ *
+ * Create new question in the Cadasta DB
+ *
+ * @param name
+ * @param label
+ * @param type_id
+ * @param p_group_id
+ * @param callback question_id
+ */
 var question_handler = function (name, label, type_id, p_group_id, callback) {
 
     var p_id = p_group_id || null;
 
     var q = 'INSERT INTO question (field_data_id, type_id, name, label,section_id,group_id) values ' +
-        '(' + survey_id + ',' + type_id + ',' + pg.sanitize(name) + ',' + pg.sanitize(label) + ',' + section_id + ',' + p_id + ') RETURNING id';
+        '(' + field_data_id + ',' + type_id + ',' + pg.sanitize(name) + ',' + pg.sanitize(label) + ',' + section_id + ',' + p_id + ') RETURNING id';
 
     pg.query(q, function (err, res) {
         if (!err) {
@@ -171,14 +194,25 @@ var question_handler = function (name, label, type_id, p_group_id, callback) {
 };
 
 // create new Group in q_group table
-var group_handler = function (async,children,parent_id, name, label, callback) {
+/**
+ *
+ * Creates new parent group in group table and returns the group_id as a parameter in the recursive function node_handler
+ *
+ * @param children
+ * @param parent_id
+ * @param name
+ * @param label
+ * @param callback group_id as 'parent_int' in node_handler()
+ */
+var group_handler = function (children,parent_id, name, label, callback) {
     if (name !== null && label !== null) {
         var q = 'INSERT INTO q_group (field_data_id,name,label,parent_id) VALUES ('
-            + survey_id + ',' + pg.sanitize(name) + ',' + pg.sanitize(label) + ',' + pg.sanitize(parent_id) + ') RETURNING id';
+            + field_data_id + ',' + pg.sanitize(name) + ',' + pg.sanitize(label) + ',' + pg.sanitize(parent_id) + ') RETURNING id';
 
         pg.queryDeferred(q)
             .then(function(res){
-                node_handler(children,res[0].id,async);
+                group_id = res[0].id;
+                node_handler(children,group_id);
             })
             .catch()
             .done();
@@ -205,9 +239,17 @@ var group_handler = function (async,children,parent_id, name, label, callback) {
 
 };
 
+/**
+ *
+ * Creates new note in Cadasta DB
+ *
+ * @param name
+ * @param label
+ * @param callback section_id
+ */
 // create new not in Section table
 var note_handler = function (name, label, callback) {
-    var q = 'INSERT INTO section(field_data_id,name,label) VALUES(' + survey_id + ',' + pg.sanitize(name) + ',' + pg.sanitize(label) + ') RETURNING id';
+    var q = 'INSERT INTO section(field_data_id,name,label) VALUES(' + field_data_id + ',' + pg.sanitize(name) + ',' + pg.sanitize(label) + ') RETURNING id';
     pg.query(q, function (err, res) {
         if (!err) {
             section_id = res[0].id; // save section id
@@ -217,6 +259,13 @@ var note_handler = function (name, label, callback) {
     });
 };
 
+/**
+ *
+ * Get question type from type table in Cadasta DB
+ *
+ * @param type
+ * @param callback type_id
+ */
 // take in type and return id from type table
 var get_type_id = function (type, callback) {
 
@@ -229,6 +278,16 @@ var get_type_id = function (type, callback) {
     })
 };
 
+/**
+ *
+ * Create new option for questions of type 'select' in Cadasta DB
+ *
+ * @param obj
+ * @param parent_question_id
+ * @param name
+ * @param label
+ * @param callback
+ */
 // create new option row in Option table
 var option_handler = function (obj, parent_question_id, name, label, callback) {
 
--- routes/data-transformer.js
+++ routes/data-transformer.js
@@ -10,24 +10,23 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 
   // clean up form data
-  var form = pg.sanitize(JSON.stringify(survey_form.form.results));
+  var results = pg.sanitize(JSON.stringify(survey_form.form.results));
 
   // process raw field data templates
   survey.processForm(survey_form.form,function(id){
 
-    var survey_id = id;
+    var field_data_id = id;
 
     // wait 20 seconds to load field data results
     var async = setTimeout(function(){
-      pg.query('SELECT * FROM cd_import_data_json(' + survey_id + ',' + form + ')',function(err,res){
-        console.log(res);
+      pg.query('SELECT * FROM cd_import_data_json(' + field_data_id + ',' + results + ')',function(err,res){
+        if(!err){
+          console.log(res);
+        }
       });
     }, 20000)
 
   });
-
-
-
 
 });
 
