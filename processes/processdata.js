8de656a510ea344a234faec3ad360c63bd960d60
DBaah <dbaah@spatialdev.com>
2015-08-17 15:05:40 -0700

    remove timer from data.load function

--- processes/processdata.js
+++ processes/processdata.js
@@ -9,7 +9,6 @@ var data = module.exports = {};
  * @callback db message (true or error)
  */
 data.load = function (field_data_id,form_data,callback) {
-    var async = setTimeout(function(){
         pg.query('SELECT * FROM cd_import_data_json(' + field_data_id + ',' + form_data + ')',function(err,res){
             if(!err){
                 callback(res);
@@ -17,5 +16,4 @@ data.load = function (field_data_id,form_data,callback) {
                 callback(err.message);
             }
         });
-    }, 10000)
 };
