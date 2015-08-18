cf3b4b780a0a8fcdecb3d838aec6ada884b97319
Ryan Whitley <apollolm@gmail.com>
2015-08-18 10:11:38 -0700

    refactoring

--- stand-alone-server/app.js
+++ stand-alone-server/app.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var pg = require('pg');
 
-var DataTransformer = require('../routes/data-transformer');
+var DataTransformer = require('./');
 
 var app = express();
 
@@ -15,7 +13,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
