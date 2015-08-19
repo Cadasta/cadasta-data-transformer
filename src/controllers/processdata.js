5b45f78072dd3d2c654dc09e88291b09429dd5e4
DBaah <dbaah@spatialdev.com>
2015-08-19 15:12:26 -0700

    update processform
    save group question parent id

--- src/controllers/processdata.js
+++ src/controllers/processdata.js
@@ -10,7 +10,7 @@ var data = module.exports = {};
  */
 data.load = function (field_data_id,form_data) {
 
-    var deferred = Q.defer()
+    var deferred = Q.defer();
 
         pg.query('SELECT * FROM cd_import_data_json(' + field_data_id + ',' + form_data + ')',function(err,res){
             if(err){
