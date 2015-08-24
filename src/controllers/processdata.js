eb206e4494f61e88714f2d2e2b4b91ce520e0d0b
Daniel Baah <dbaah@spatialdev.com>
2015-08-24 12:25:42 -0700

    data.load grabs field data id from the DB before loading form data

--- src/controllers/processdata.js
+++ src/controllers/processdata.js
@@ -4,22 +4,57 @@ var data = module.exports = {};
 
 /**
  *
- * @param field_data_id
- * @param form_data (CJF data - array of objects)
- * @callback db message (true or error)
+ * @param form_data
  */
-data.load = function (field_data_id,form_data) {
+data.load = function (form_data) {
+    getFieldDataId()
+        .then(function(field_data_id){
+            return loadFieldData(field_data_id,form_data);
+        })
+};
+
+/**
+ *
+ * @param field_data_id
+ * @param form_data
+ * @returns {*|promise}
+ */
+function loadFieldData(field_data_id,form_data){
 
     var deferred = Q.defer();
 
-        pg.query('SELECT * FROM cd_import_data_json(' + field_data_id + ',' + form_data + ')',function(err,res){
-            if(err){
-
-                deferred.reject(err);
-            } else {
-                deferred.resolve(res);
-            }
-        });
+    pg.query('SELECT * FROM cd_import_data_json(' + field_data_id + ',' + form_data + ')',function(err,res){
+        if(err){
+            deferred.reject(err);
+        } else {
+            deferred.resolve(res);
+        }
+    });
 
     return deferred.promise;
-};
+}
+
+/**
+ * Get id of latest survey from field_data table (assuming survey doesn't change)
+ * @returns {*|promise}
+ */
+function getFieldDataId (){
+
+    var deferred = Q.defer();
+
+    var sql = 'SELECT id FROM field_data ORDER BY id DESC LIMIT 1';
+
+    pg.query(sql, function (err, res) {
+
+        if(err) {
+            deferred.reject(err);
+        } else {
+            deferred.resolve(res[0].id);
+        }
+
+    });
+
+    return deferred.promise;
+
+}
+
